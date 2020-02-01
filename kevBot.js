fb06279958c6e6a299a6c80ac7cddec3faa85b06
Keven McDowell <kpmcdowellmo@gmail.com>
2020-02-01 15:42:04 -0600

    continuing to work on converting to class based impl

--- kevBot.js
+++ kevBot.js
@@ -1,10 +1,11 @@
 const request = require("request"),
   twitterAuth = require("../twitterauth.json"),
   Twitter = require("twitter"),
-  auth = require("../auth.json")
+  auth = require("../auth.json"),
   botMessages = require("../kevbotMessage.json"),
   ytsr = require("ytsr"),
-  ytdl = require("ytdl-core");
+  ytdl = require("ytdl-core"),
+  chalk = require("chalk");
 
 const TwitterAPI = new Twitter({
   consumer_key: twitterAuth.consumer_key,
@@ -19,15 +20,15 @@ const Discord = require("discord.js"),
   request = require("request"),
   kevBot = new Discord.Client();
 
-  const botMsgEnum = {
-    ".godEmperor": "theDon",
-    ".Norris": "chuckNorrisJoke",
-    ".help": "getHelp",
-    ".play": "checkStream",
-    ".skip": true, // WIP
-    ".search": true,
-    ".volume": true
-  };
+const botMsgEnum = {
+  ".godEmperor": "_theDon",
+  ".Norris": "_chuckNorrisJoke",
+  ".help": "_getHelp",
+  ".play": "",
+  ".skip": true, // WIP
+  ".search": true,
+  ".volume": true
+};
 
 class kevBot {
   constructor() {
@@ -36,80 +37,59 @@ class kevBot {
     this._addBotListeners();
   }
 
-  _messageListener(message){
+  _messageListener(message) {
     console.log(message.content);
-      if (message.author.bot) {
-        return;
-      } else {
-        if (message.content.substring(0, 2) == "k.") {
-          // Checking if the message begins with "k." to determine when the bot is being utilized.
-          let args = message.content.substring(1).split(" "), // Builds and array of arguments for the current command.
-            cmd = args[0]; // Grabs the command from the list of arguments, which is always the first argument.
-          args = args.splice(1);
-          console.assert("USER COMMAND: ", cmd);
-          if (botMsg[cmd]) {
-            this[botMsgEnum[cmd]](message, args);
-          } else {
-            message.channel.send(
-              'Invalid command entered. Please use "k.help" to get a list of commands.'
-            );
-          }
+    if (message.author.bot) {
+      return;
+    } else {
+      if (message.content.substring(0, 2) == "k.") {
+        // Checking if the message begins with "k." to determine when the bot is being utilized.
+        let args = message.content.substring(1).split(" "), // Builds and array of arguments for the current command.
+          cmd = args[0]; // Grabs the command from the list of arguments, which is always the first argument.
+        args = args.splice(1);
+        console.assert("USER COMMAND: ", cmd);
+        if (botMsg[cmd]) {
+          this[botMsgEnum[cmd]](message, args);
+        } else {
+          message.channel.send(
+            'Invalid command entered. Please use "k.help" to get a list of commands.'
+          );
         }
       }
+    }
   }
 
-  _errorListener(err){
-    console.log("error", err);
+  _errorListener(err) {
+    console.log(chalk.red("error", err));
   }
 
-  _readyListener(){
-    console.log("KevBot Ready");
+  _readyListener() {
+    console.log(chalk.green("KevBot Ready"));
   }
 
-  _addBotListeners(){
+  _addBotListeners() {
     this.bot.on("message", message => this._messageListener(message));
     this.bot.on("error", err => this._errorListener(err));
     this.bot.on("ready", () => this._readyListener());
   }
-}
 
-const kevBotFunctions = {
-  checkStream: (message, args) => {
-    searcher
-      .search(args.join(" "), {
-        type: "video"
-      })
-      .then(result => {
-        if (result.first.liveBroadcastContent === "live") {
-          setTimeout(() => {
-            message.channel.send(
-              "Live Stream selected. Leaving to prevent DDOS hell"
-            );
-            kevBot.music.bot.leaveFunction(message);
-          }, 2000);
-        }
-      })
-      .catch(err => {
-        console.log("error occured while searching videos.");
-      });
-  },
-  theDon: message => {
+  _theDon(message) {
     const options = {
       screen_name: "realDonaldTrump",
       count: 5
     };
     TwitterAPI.get(
-      "statuses/user_timeline", {
+      "statuses/user_timeline",
+      {
         screen_name: "realDonaldTrump",
         count: 5
       },
-
       (error, tweets, response) => {
         if (error) {
           kevBot.channel.send("Can't get your dose of the Don... :(");
           console.log(error);
         } else {
-          tweets.forEach(function (tweet) {
+          tweets.forEach(function(tweet) {
             let responseMessage =
               "User: " + tweet.user.name + "\n" + "Content: " + tweet.text;
             message.channel.send(responseMessage);
@@ -117,8 +97,14 @@ const kevBotFunctions = {
         }
       }
     );
-  },
-  chuckNorrisJoke: message => {
+  }
+
+  _getHelp(message) {
+    let helpMessage = "I'm working on it.";
+    message.channel.send(botMessages.help);
+  }
+
+  _chuckNorrisJoke(message) {
     request(
       "https://api.chucknorris.io/jokes/random",
       (error, response, body) => {
@@ -133,13 +119,5 @@ const kevBotFunctions = {
         }
       }
     );
-  },
-  getHelp: message => {
-    let helpMessage = "I'm working on it.";
-    message.channel.send(botMessages.help);
-  },
-  searchSong: async query => await ytsr(query),
-  playSong: url =>
-};
-
-module.exports = kevBotFunctions;
+  }
+}
